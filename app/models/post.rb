6979dea1ec4bc2793d215fa535e411967e8ad9cf
Vladimir2706 <liv17171@gmail.com>
2018-03-11 09:29:44 +0200

    Set up paperclipp for image uploading

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+  has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   belongs_to :user
 end
