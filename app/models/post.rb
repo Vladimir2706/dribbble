564ac99a99624b93277e1febb9d0f6a982ae57c8
Vladimir2706 <liv17171@gmail.com>
2018-03-11 09:55:35 +0200

    Add comments to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   belongs_to :user
+  has_many :comments
 end
